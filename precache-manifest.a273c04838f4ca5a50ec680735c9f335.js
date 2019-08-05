07ece88f9f9373ee82d1f5a36d74de48a655242d
Sean <kaido0121@gmail.com>
2019-08-05 15:54:51 +0800

    Updates

---
+++ precache-manifest.a273c04838f4ca5a50ec680735c9f335.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1f0699ea660a5ecaa0b504b7c1ad546",
+    "url": "/react-SPA/index.html"
+  },
+  {
+    "revision": "fa2b137fdf88bf4cf80b",
+    "url": "/react-SPA/static/css/3.c473469c.chunk.css"
+  },
+  {
+    "revision": "a05a9482abd2dca049be",
+    "url": "/react-SPA/static/css/main.89beac6c.chunk.css"
+  },
+  {
+    "revision": "31b39103748e582fb780",
+    "url": "/react-SPA/static/js/2.7dd9ae67.chunk.js"
+  },
+  {
+    "revision": "fa2b137fdf88bf4cf80b",
+    "url": "/react-SPA/static/js/3.7eb14e92.chunk.js"
+  },
+  {
+    "revision": "a05a9482abd2dca049be",
+    "url": "/react-SPA/static/js/main.f66db6b4.chunk.js"
+  },
+  {
+    "revision": "acc4dc63c1b2f0f1f9e9",
+    "url": "/react-SPA/static/js/runtime~main.15b803da.js"
+  }
+]);
